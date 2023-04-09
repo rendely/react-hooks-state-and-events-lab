65eacbdb2686825fa1eabd059879d514ab53bd13
matthew rendely <matthew.rendely@gmail.com>
2023-04-09 09:49:44 -0700

    use hooks for add and remove from cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setCart] = useState(false);
+  const cartClass = inCart ? 'in-cart' : '';
+  function handleClick(){
+    setCart(!inCart);
+  }
+
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart ? 'remove' : 'add'} onClick={handleClick}>{inCart? 'Remove From Cart' : 'Add to Cart'}</button>
     </li>
   );
 }
